0dc6101a26d7e3062f44a1d40c3d2fc99ca5c9fd
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2020-06-22 22:38:35 +0200

    Added json dumper

--- ipxactral/ipxact.py
+++ ipxactral/ipxact.py
@@ -1,30 +1,31 @@
 import xml.etree.ElementTree as ET
-
 import json
+
+def node_to_dict(node):
+    data = {}
+    data['tag'] = node.tag
+
+    text = node.text
+    if text:
+        text = text.strip()
+    if text == "":
+        text = None
+    data['text'] = text
+
+    data['attrib']= node.attrib
+    data['children'] = [node_to_dict(c) for c in node]
+    return data
 
 class Ipxact(object):
     def __init__(self,filename):
         self.filename = filename
+        self.xmlroot = None
 
     def parse(self):
         tree = ET.parse(self.filename)
-        self.root = tree.getroot()
-        self.Santize()
+        self.xmlroot = tree.getroot()
+        self.json = node_to_dict(self.xmlroot)
 
-    def rSantize(self, node):
-        """
-        Remove spaces and newlines from element text
-        """
-        if node.text:
-            node.text = node.text.strip()
-        if node.text == "":
-            node.text = None
-
-        for child in node:
-            self.rSantize(child)
-
-    def Santize(self):
-        self.rSantize(self.root)
-
-    def json(self):
-        pass
+    def dump_jsonify(self,jsonfile):
+        with open(jsonfile, 'w') as outfile:
+            json.dump(self.json, outfile,indent=2)
--- ipxactral/main.py
+++ ipxactral/main.py
@@ -9,8 +9,12 @@ def main():
     parser = argparse.ArgumentParser(description='ipxact parser and generator')
 
     parser.add_argument('ipxactfile')
+    parser.add_argument('--json', nargs='?', help='Generate json')
 
     args = parser.parse_args()
 
     o = ipxact.Ipxact(args.ipxactfile)
     o.parse()
+
+    if args.json:
+        o.dump_jsonify(args.json)
